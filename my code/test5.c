b1742181b8c726c6f6345356696e15349aa2c808
Krollbotid <krollbotid@gmail.com>
2023-11-25 17:54:24 +0300

    third try

--- my code/test5.c
+++ my code/test5.c
@@ -8,8 +8,6 @@ void print_dct_blocks(const char *filename) {
         return;
     }
 
-    // Open the input file for reading in binary mode.
-
     struct jpeg_decompress_struct cinfo;
     struct jpeg_error_mgr jerr;
 
@@ -19,36 +17,29 @@ void print_dct_blocks(const char *filename) {
     jpeg_read_header(&cinfo, TRUE);
     jpeg_start_decompress(&cinfo);
 
-    // Set up JPEG decompression structure, error handling, and start decompression process.
-
     JDIMENSION MCU_width = cinfo.comp_info[0].width_in_blocks;
     JDIMENSION MCU_height = cinfo.comp_info[0].height_in_blocks;
 
-    // Retrieve dimensions of Minimum Coded Unit (MCU) in width and height.
-
     JDIMENSION block_row;
     while (cinfo.output_scanline < cinfo.output_height) {
-        JBLOCKARRAY buffer = (*cinfo.mem->alloc_barray)((j_common_ptr) &cinfo, JPOOL_IMAGE, MCU_width * DCTSIZE2);
+        JSAMPIMAGE buffer = (*cinfo.mem->alloc_sarray)((j_common_ptr) &cinfo, JPOOL_IMAGE, MCU_width * DCTSIZE2, MCU_height);
         jpeg_read_raw_data(&cinfo, buffer, MCU_height);
         for (block_row = 0; block_row < MCU_height; block_row++) {
-            JCOEFPTR block = buffer[0][block_row];
+            JSAMPROW row = buffer[0][block_row];
             for (JDIMENSION i = 0; i < MCU_width; i++) {
                 for (JDIMENSION j = 0; j < DCTSIZE2; j++) {
-                    printf("%d ", block[i * DCTSIZE2 + j]);
+                    printf("%d ", row[i * DCTSIZE2 + j]);
                 }
                 printf("\n");
             }
             printf("\n");
         }
-        (*cinfo.mem->free_barray)((j_common_ptr) &cinfo, buffer, MCU_height);
+        (*cinfo.mem->free_sarray)((j_common_ptr) &cinfo, buffer, MCU_height);
     }
-
-    // Loop through the image data, reading the raw data and printing DCT blocks.
 
     jpeg_finish_decompress(&cinfo);
     jpeg_destroy_decompress(&cinfo);
     fclose(infile);
-    // Finish decompression process and release decompression structure.
 }
 
 int main(int argc, char *argv[]) {
@@ -60,5 +51,3 @@ int main(int argc, char *argv[]) {
     print_dct_blocks(filename);
     return 0;
 }
-
-// Main function to check for command-line argument and call the print_dct_blocks function.
